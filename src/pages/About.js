f396d9f101155e861afe62eba66cdeed99df312e
DevelopPaPa <developpapa@MacBook-Pro-DevelopPaPa.local>
2020-10-16 22:10:17 +0300

    “initial”

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from "react";
+
+export const About = ()=> (
+
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">React приложение</h1>
+            <p className="lead">
+                Версия приложения <strong>1.10.17.42</strong>
+            </p>
+        </div>
+    </div>
+)
